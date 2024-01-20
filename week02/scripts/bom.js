ccd0d1d246cf8917bc97e30ee5e6884ab8c8b623
nstod2 <nancyastoddart@gmail.com>
2024-01-20 14:43:39 -0700

    Changes to bom.html

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,23 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.document.addEventListener('click', () => {
+button.addEventListener('click', () => {
     if (input.value != '') {
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
         li.textContent = input.value;
-        deleteButton.textContent = ''
+        deleteButton.textContent = ':x:';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.focus();
+        input.value = '';
+    } else { 
+        input.focus();
     }
-})
+
+});
+
