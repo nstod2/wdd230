0992b1ba470112c4cc1b7aec88c48eaf8f01400d
nstod2 <nancyastoddart@gmail.com>
2024-01-24 19:35:11 -0700

    Lazy Loading Assignment

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const day = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const hour = today.getHours();
+const min = today.getMinutes();
+const sec = today.getSeconds();
+let dateTime = month + '/' + day + '/' + year + ' ' + hour + ':' + min + ':' + sec;
+document.querySelector('#lastModified').textContent = `Last Modification: ${dateTime}`;
+document.querySelector('#copyright').innerHTML = `&copy ${year}`;
