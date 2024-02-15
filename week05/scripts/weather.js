5a808b7bb25d0b8f19a00fe3672beee2a7f6effb
nstod2 <nancyastoddart@gmail.com>
2024-02-15 09:35:06 -0700

    Fixed units for temp

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=fc750def0ad8258e60aec92a95a39b23&unit=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=fc750def0ad8258e60aec92a95a39b23&units=imperial';
 
 async function apiFetch() {
     try {
