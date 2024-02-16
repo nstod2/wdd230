79a0a13a56c7380667fe0c1ae0582b08c6afaf8a
nstod2 <nancyastoddart@gmail.com>
2024-02-15 19:28:37 -0700

    Changes links.js

--- scripts/links.js
+++ scripts/links.js
@@ -4,9 +4,19 @@ const linksURL = "https://nstod2.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-    displayWeeks(data.weeks);
-
+    displayLinks(data.weeks);
 }
 
-getLinks();
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let numLinks = 1;
+        
+        week.forEach((links) =>{
+            numLinks = 1;
+
+        })
+        
+    });
+}
+
+    getLinks();
