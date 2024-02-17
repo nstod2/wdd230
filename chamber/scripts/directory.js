360fbd1f4fa4fe5164fea0a0416753035a2094a3
nstod2 <nancyastoddart@gmail.com>
2024-02-16 17:35:55 -0700

    Fixes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,17 @@
-const url = 
+const url = "https://nstod2.github.io/wdd230/chamber/data/members.json";
+const mcards = document.querySelector('#member-cards');
+
+async function getMemberData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.members);
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        
+        let card = document.createElement('section');
+        let 
+    });
+}
